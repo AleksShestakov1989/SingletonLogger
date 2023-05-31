8cbea3a5de0b7c7c1bf5cfce619492e088c8aa5b
Aleks <shestakov.aleksey8989@gmail.com>
2023-05-31 16:14:29 +0300

    Initial commit

---
+++ src/main/java/Filter.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Filter {
+    protected int treshold;
+
+    public Filter(int treshold) {
+        this.treshold = treshold;
+    }
+
+    public List<Integer> filterOut(List<Integer> source) {
+        Logger logger = Logger.getInstance();
+        List<Integer> filterOut = new ArrayList<>();
+        for (int x : source) {
+            if (x > treshold) {
+                logger.log("Элемент " + x + " не проходит");
+            } else {
+                logger.log("Элемент " + x + " проходит");
+                filterOut.add(x);
+            }
+        }
+        return filterOut;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.log("Запускаем программу");
+        logger.log("Просим пользователя ввести входные данные для списка");
+        System.out.print("Введите размер списка: ");
+        Scanner scanner = new Scanner(System.in);
+        int longValue = scanner.nextInt();
+        System.out.print("Введите верхнюю границу для значений: ");
+        int maxValue = scanner.nextInt();
+        logger.log("Создаём и наполняем список");
+        List<Integer> list = new Random()
+                .ints(0, maxValue)
+                .limit(longValue)
+                .boxed()
+                .toList();
+        System.out.print("Вот случайный список: ");
+        list.forEach(value -> System.out.print(value + " "));
+        System.out.println();
+        logger.log("Просим пользователя ввести входные данные для фильтрации");
+        System.out.print("Введите порог для фильтра: ");
+        int treshold = scanner.nextInt();
+        logger.log("Запускаем фильтрацию");
+        Filter filter = new Filter(treshold);
+        List<Integer> listByFilter = filter.filterOut(list);
+
+
+        logger.log("Прошло фильтр " + listByFilter.size() + " элемента из " + list.size());
+
+        logger.log("Выводим результат на экран");
+        System.out.print("Отфильтрованный список: ");
+        listByFilter.forEach(value -> System.out.print(value + " "));
+        System.out.println();
+
+        logger.log("Завершаем программу");
+    }
+}
